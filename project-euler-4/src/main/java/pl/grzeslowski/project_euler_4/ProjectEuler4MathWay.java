5540fe9becbda3c0bb1d7d7efa25d5b18a441e1f
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 22:34:13 +0100

    Implementation

--- project-euler-4/src/main/java/pl/grzeslowski/project_euler_4/ProjectEuler4MathWay.java
+++ project-euler-4/src/main/java/pl/grzeslowski/project_euler_4/ProjectEuler4MathWay.java
@@ -2,9 +2,16 @@ package pl.grzeslowski.project_euler_4;
 
 public class ProjectEuler4MathWay implements ProjectEuler4 {
 
-    public long getDifference(int endNumber) {
-        // TODO Auto-generated method stub
-        return 0;
+    public long getDifference(final int endNumber) {
+        long value = 0;
+
+        for (int i = 1; i <= endNumber; i++) {
+            for (int k = i + 1; k <= endNumber; k++) {
+                value += 2 * i * k;
+            }
+        }
+
+        return value;
     }
 
 }
