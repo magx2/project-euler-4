279411e38faf4f94f571cb7aabe8039b5a981d2a
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 22:34:18 +0100

    Main

--- project-euler-4/src/main/java/pl/grzeslowski/project_euler_4/Main.java
+++ project-euler-4/src/main/java/pl/grzeslowski/project_euler_4/Main.java
@@ -2,9 +2,12 @@ package pl.grzeslowski.project_euler_4;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(final String[] args) {
+        final int endNumber = Integer.parseInt(args[0]);
+        
+        final ProjectEuler4MathWay projectEuler4MathWay = new ProjectEuler4MathWay();
 
+        System.out.println(projectEuler4MathWay.getDifference(endNumber));
     }
 
 }
